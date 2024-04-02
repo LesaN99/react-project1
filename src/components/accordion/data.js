6563a1d7234bad28dd5faf48214f5143c7183c9b
LesaN99 <149886844+LesaN99@users.noreply.github.com>
2024-04-02 17:19:44 +0200

    Changed the data,js file content and  added new styling to the page.

---
+++ src/components/accordion/data.js
@@ -0,0 +1,29 @@
+const data =[
+{
+id : "1",
+question: "Who is Nicki Minaj?",
+answer: 
+"Onika Tanya Maraj-Petty(born December 8, 1982),known professionally as Nicki Minaj is a Trinidadian-born rapper, singer and songwriter."
+},
+{
+id : "2",
+question: "What is so special about Nicki Minaj?",
+answer:
+"She has a bold and unapologetic personality. She is known for her confident and empowering lyrics, her musical versatility, animated rap flow and various alter egos."
+},
+
+{
+id : "3",
+question: "How many awards does Nicki have?",
+answer:
+"Overall, she has won twelve awards out of thirty-three nominations."
+},
+{
+id : "4",
+question: "What is Nicki Minaj's motto?",
+answer: 
+"The qoute by Nicki Minaj,\"I'm not here to fit into your wold. I'm here to create my own\", encapsulates her unapologetic approach to life and career."
+},
+];
+
+export default data;
